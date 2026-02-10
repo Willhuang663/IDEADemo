b8220fced70ad11239d8c81d3cd8d0c286d29e49
HHY <1559677663@qq.com>
2026-02-10 10:26:19 +0800

    third commit

---
+++ src/com/hhy/test/Demo07.java
@@ -0,0 +1,9 @@
+package com.hhy.test;
+
+public class Demo07 {
+    static void main() {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git22");
+    }
+}
